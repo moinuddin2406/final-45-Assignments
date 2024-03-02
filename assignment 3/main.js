e15a3a1164f75cc5a11ef2efc5f41671a1e8f873
moinuddin2406 <161001044+moinuddin2406@users.noreply.github.com>
2024-03-02 15:27:47 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// sroring the famous quote and its author
+let quote = "To win that game just remain in the game";
+let author = "moinuddin";
+// printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ assignment 3/main.ts
@@ -0,0 +1,5 @@
+// sroring the famous quote and its author
+let quote:string = "To win that game just remain in the game"
+let author:string ="moinuddin";
+// printing the message
+console.log(`"${quote}" - ${author}`);
